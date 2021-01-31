e51d72b7d52b6825e61af6e9039320b70f61d5e3
nessx <cf17nestor.santana@iesjoandaustria.org>
2021-01-31 02:42:40 +0100

    Login con google OK

--- app/src/main/java/com/team/projectcatalina/MainActivity.java
+++ app/src/main/java/com/team/projectcatalina/MainActivity.java
@@ -1,86 +1,95 @@
 package com.team.projectcatalina;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 import com.team.projectcatalina.sp.sp_manager;
 import com.team.projectcatalina.clases.usuario;
 import com.google.android.material.snackbar.Snackbar;
 
-import java.util.Locale;
-
 
 public class MainActivity extends AppCompatActivity {
     //declaracion de variables junto a su tipo
-    Button logbtn;
-    EditText loguser, logpass;
-    private CheckBox checkBoxRememberMe;
     private sp_manager s_preferences;
+    private static final String TAG = "TEST";
+
+    private static final int RC_SIGN_IN = 101;
+    SignInButton signInButton;
+    GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
         s_preferences = new sp_manager(getApplicationContext());
+
         /*
         //test language preferences
         if(!new sp_manager(this).noLang()){
             setAppLocale(s_preferences.getLang());
         }*/
 
-        //Checkbox
-        checkBoxRememberMe = (CheckBox) findViewById(R.id.checkBox);
+        //GOOGLE SING
+        //Initializing Views
+        signInButton = findViewById(R.id.signInButton);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder (GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken (getString (R.string.default_web_client_id))
+                .requestEmail ()
+                .build ();
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        //END
+
         //si el usuario no esta loggeado
-        if (!new sp_manager(this).isUserLogedOut()) {
-            //el usuario y la contraseña estan guardados en preferences
+        if (!new sp_manager(this).isUserGoogleLogedOut()) {
             gotomenu();
         }
 
-        //De declaracion de variables
-        loguser = findViewById(R.id.loguser);
-        logpass = findViewById(R.id.logpass);
-        logbtn = findViewById(R.id.logbtn);
-
-        //codigo del botton
-        logbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                usuario user = new usuario();
-                if ((loguser.getText().toString().trim().equals(user.getuser())) && (logpass.getText().toString().trim().equals(user.getpasswd()))){
-                    if (checkBoxRememberMe.isChecked())
-                        saveLoginDetails(loguser.getText().toString().trim(), logpass.getText().toString().trim());
-                    SharedPreferences sp = getSharedPreferences("LoginDetails" ,Context.MODE_PRIVATE);
-                    s_preferences.getUser();
-                    s_preferences.getPass();
-                    gotomenu();
-                }
-                else{
-                    Snackbar.make(v, "ERROR AL INICIAR SESION", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            }
-        });
-    }
-    private void saveLoginDetails(String user, String password) {
-        new sp_manager(this).saveLoginDetails(user, password);
     }
 
     public void gotomenu(){
-        Intent intent = new Intent (MainActivity.this, MainActivity.class);
+        Intent intent = new Intent (MainActivity.this, start.class);
         startActivity(intent);
     }
 
+    //LANG OPTIONS
     /*
     private void setAppLocale(String localeCode){
         Resources resources = getResources();
@@ -92,6 +101,61 @@ public class MainActivity extends AppCompatActivity {
         resources.updateConfiguration(configuration, displayMetrics);
     }
      */
+
+    //FIREBASE LOGIN GOOGLE ACCOUNT
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Toast.makeText(this,"Loggeado correctamente " + account.getDisplayName(), Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                s_preferences.SaveGoogleLoginDetails(account.getId());
+                SharedPreferences sp = getSharedPreferences("GoogleLoginDetails" ,Context.MODE_PRIVATE);
+                s_preferences.getUser();
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this,"Google sign in failed "+e.toString(), Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "Google sign in failed "+e.toString());
+                // ...
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            updateUI(null);
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+        Intent intent = new Intent(MainActivity.this, start.class);
+        startActivity(intent);
+    }
 
 
 }
--- app/src/main/java/com/team/projectcatalina/sp/sp_manager.java
+++ app/src/main/java/com/team/projectcatalina/sp/sp_manager.java
@@ -3,6 +3,7 @@ package com.team.projectcatalina.sp;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.util.Log;
 
 /**
  * Class for Shared Preference
@@ -23,6 +24,14 @@ public class sp_manager {
         editor.commit();
     }
 
+    //GOOGLE
+    public void SaveGoogleLoginDetails(String id) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("GoogleLoginDetails", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("ID", id);
+        editor.commit();
+    }
+
     public void savelangDetails(String lang) {
         SharedPreferences sharedPreferences = context.getSharedPreferences("LangDetails", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
@@ -30,8 +39,8 @@ public class sp_manager {
         editor.commit();
     }
     public String getLang() {
-        SharedPreferences sharedPreferences = context.getSharedPreferences("LangDetails", Context.MODE_PRIVATE);
-        return sharedPreferences.getString("Lang", "");
+        SharedPreferences sharedPreferences = context.getSharedPreferences("GoogleLoginDetails", Context.MODE_PRIVATE);
+        return sharedPreferences.getString("ID", "");
     }
 
     public boolean noLang() {
@@ -56,11 +65,23 @@ public class sp_manager {
         return isUserEmpty || isPasswordEmpty;
     }
 
+    public boolean isUserGoogleLogedOut() {
+        SharedPreferences sharedPreferences = context.getSharedPreferences("GoogleLoginDetails", Context.MODE_PRIVATE);
+        boolean isIdEmpty = sharedPreferences.getString("ID", "").isEmpty();
+        Log.i("SHARED", "SP "+isIdEmpty);
+        return isIdEmpty;
+    }
+
     public void reset() {
-        SharedPreferences login = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
-        SharedPreferences lang = context.getSharedPreferences("LangDetails", Context.MODE_PRIVATE);
-        login.edit().clear().commit();
-        lang.edit().clear().commit();
+        SharedPreferences sharedPreferences = context.getSharedPreferences("GoogleLoginDetails", Context.MODE_PRIVATE);
+        sharedPreferences.edit().clear().commit();
+
+        //SharedPreferences login = context.getSharedPreferences("LoginDetails", Context.MODE_PRIVATE);
+        //SharedPreferences lang = context.getSharedPreferences("LangDetails", Context.MODE_PRIVATE);
+        //SharedPreferences Glang = context.getSharedPreferences("GoogleLoginDetails", Context.MODE_PRIVATE);
+        //login.edit().clear().commit();
+        //ang.edit().clear().commit();
+        //Glang.edit().clear().commit();
     }
 
 }
