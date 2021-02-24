15bc0fcabd29f31b2fd0d77d4d80b81fada7883f
martingrub <eric27san@gmail.com>
2021-02-24 19:38:17 +0100

    PREMIUM PLAN

--- app/src/main/java/com/team/projectcatalina/fragments/premium.java
+++ app/src/main/java/com/team/projectcatalina/fragments/premium.java
@@ -61,6 +61,6 @@ public class premium extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_premiumV, container, false);
+        return inflater.inflate(R.layout.fragment_premium, container, false);
     }
 }
