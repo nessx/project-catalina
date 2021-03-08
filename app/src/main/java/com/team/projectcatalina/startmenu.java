34c2b4d3d2bfd773721730f6cc524a3243abd5f5
nessx <cf17nestor.santana@iesjoandaustria.org>
2021-03-08 01:48:49 +0100

    IMPLEMENTS DISTANCES NODE FROM FIREBASE

--- app/src/main/java/com/team/projectcatalina/startmenu.java
+++ app/src/main/java/com/team/projectcatalina/startmenu.java
@@ -3,6 +3,7 @@ import androidx.annotation.NonNull;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -14,10 +15,16 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.team.projectcatalina.clases.Dijkstra;
+import com.team.projectcatalina.clases.Edge;
 import com.team.projectcatalina.clases.Vert;
 import com.team.projectcatalina.clases.user;
 import com.team.projectcatalina.fragments.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -93,36 +100,65 @@ public class startmenu extends AppCompatActivity {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("SECURE_TRANSPORT/Estaciones");
 
+        DatabaseReference myRef2 = database.getReference("SECURE_TRANSPORT/NODOS");
+
         //Writing Hashmap
         Map<String, Object> mHashmap = new HashMap<>();
+        /*
+        mHashmap.put("nodo"+i+"/FROM", vertexMap.get(vFrom).toString());
+        mHashmap.put("nodo"+i+"/TO", vertexMap.get(vTo).toString());
+        mHashmap.put("nodo"+i+"/WEIGHT", weight);
+        myRef.updateChildren(mHashmap);
+         */
 
         //Log.i("logTes","chivato "+ listado.get(i));
-        //mHashmap.put(listado.get(i)+"/Estado", "DISPONIBLE");
-        myRef.updateChildren(mHashmap);
 
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 Map<String, Object> value = (Map<String, Object>) dataSnapshot.getValue();
+                int i = 0;
 
 
                 for (Map.Entry entry : value.entrySet()) {
-                    Log.d("FIREBASEDB", "key: " + entry.getKey() + "; value: " + entry.getValue());
+                    //Log.d("FIREBASEDB", "key: " + entry.getKey() + "; value: " + entry.getValue());
 
                     Vert v = new Vert(entry.getKey().toString());
                     //spinnerArray.add(entry.getKey().toString());
                     lista.add(v);
-                    for(int i=0;i<lista.size();i++){
-                        Log.d("FIREBASED", "Lista " + lista.get(i));
-                    }
-                    Log.d("FIREBASEDB", "Paradas " + entry.getKey().toString());
+                    Log.d("FIREBASEDB", "Lista " + lista.get(i));
+                    i++;
+                }
+                //Log.d("FIREBASEDB", "Value is: " + value.get("Hospital_clinic"));
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("FIREBASEDB", "Failed to read value.", error.toException());
+            }
+        });
+
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot data : dataSnapshot.getChildren()) {
+                    String vFrom = data.child("FROM").getValue(String.class);
+                    String vTo = data.child("TO").getValue(String.class);
+                    int weight = data.child("WEIGHT").getValue(int.class);
 
                     //v.addNeighbour(new Edge(weight, vertexMap.get(vFrom), vertexMap.get(vTo)));
-                }
+                    Log.d("FIREBASEDB", "FROM:" + vFrom + "; TO:" + vTo + "; WEIGHT;"+ weight);
 
-                //log estus
-                //Log.d("FIREBASEDB", "Value is: " + value.get("Hospital_clinic"));
+                    //TEST
+                    /*
+                    Dijkstra.getShortestP(lista.get(0));
+                    for(int x=0;x<lista.size();x++){
+                        Log.i("FIREBASEDB","paradas minimas "+ Dijkstra.getShortestP(lista.get(x)));
+                    } */
+
+                }
 
             }
 
@@ -132,6 +168,10 @@ public class startmenu extends AppCompatActivity {
                 Log.w("FIREBASEDB", "Failed to read value.", error.toException());
             }
         });
+
+        //Dijkstra.ShortestP(lista.get(0));
+        //Toast.makeText(getApplicationContext(),"TEXTO SELECCIONADO "+lista.get(0).toString(), Toast.LENGTH_SHORT).show();
+
 
         return lista;
     }
