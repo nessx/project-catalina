028bd613e9db9f863d0e4c2c57a757779d8a9206
cf17jeremy <cf17jeremy.torres@iesjoandaustria.org>
2021-03-14 05:49:38 +0100

    estado pruebas firebase 2.0

--- app/src/main/java/com/team/projectcatalina/clases/RecyclerViewAdapter.java
+++ app/src/main/java/com/team/projectcatalina/clases/RecyclerViewAdapter.java
@@ -13,7 +13,6 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.team.projectcatalina.R;
 import com.team.projectcatalina.fragments.ParadasFragment;
@@ -23,7 +22,8 @@ import java.util.ArrayList;
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
     private ArrayList<Vert> array_paradas;
     private ParadasFragment context;
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference database = FirebaseDatabase.getInstance().getReference("SECURE_TRANSPORT/Estaciones/");
+    private TextView estado;
 
     public RecyclerViewAdapter(ParadasFragment con, ArrayList<Vert> arrI){
         array_paradas = arrI;
@@ -49,17 +49,30 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         return array_paradas.size();
     }
 
-    public String Query (){
-        Query estado =
-                FirebaseDatabase.getInstance().getReference()
-                    .child("Estaciones")
-                    .orderByValue()
-                    .equalTo("DISPONIBLE");
-        String estadoS = estado.toString();
-        Log.i("ESTADOS", "estado? " + estadoS);
-        return estadoS;
+    public void Query (){
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot data : dataSnapshot.getChildren()){
+                    String parada = data.getKey();
+                    String status = data.child("Estado").getValue(String.class);
+                    Log.d("ESTADOS", "ESTADO:" + status + "; PARADA:" + parada);
+                    if (status == "DISPONIBLE") {
+                        estado.setBackgroundResource(R.drawable.circle_green);
+                    } else {
+                        estado.setBackgroundResource(R.drawable.circle_red);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
-    public void Querynessx(){
+
+    /*public void Querynessx(){
         DatabaseReference reference = database.getReference("SECURE_TRANSPORT/Estaciones/");
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -78,20 +91,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
             }
         });
-    }
+    }*/
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView etiquetaNom,estado;
+        TextView etiquetaNom;
         CardView layout;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             estado = itemView.findViewById(R.id.estado);
-            if (Query() == "DISPONIBLE"){
-                estado.setBackgroundResource(R.drawable.circle_green);
-            }else {
-                estado.setBackgroundResource(R.drawable.circle_red);
-            }
+            Query();
             etiquetaNom = itemView.findViewById(R.id.itemListIncidencia);
             layout = itemView.findViewById(R.id.layout);
         }
